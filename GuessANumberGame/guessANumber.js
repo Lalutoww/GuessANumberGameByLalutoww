5be9e6fbcfdb4d5719bb25504ffbe3cc3472d69a
Martin Lalutov <martinlalutov00@gmail.com>
2022-09-28 13:02:15 +0300

    Added difficulty system
    Version 1.5;
    Made by lalutoww;

--- GuessANumberGame/guessANumber.js
+++ GuessANumberGame/guessANumber.js
@@ -1,33 +1,64 @@
-function guessANumber(){
-    const readline = require('readline').createInterface({
+function guessANumber() {
+    const readline = require("readline").createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
     });
-    let computerGuess = Math.floor(Math.random()* 100);
+    let task = "";
+    let level;
     let guess;
+    let difficulty = '';
+    let computerGuess = Math.random();
+    readline.question("Choose difficulty (1, 2 or 3)",(option) => {
+        switch (option) {
+            case '1': difficulty = '1'; task = "0-100"; computerGuess = Math.floor(computerGuess * 100); game(); break;
+            case '2': difficulty = '2'; task = "0-150"; computerGuess = Math.floor(computerGuess * 150); game(); break;
+            case '3': difficulty = '3'; task = "0-200"; computerGuess = Math.floor(computerGuess * 200); game(); break;
+        }
+    });
 
-    let question = function(){
-        readline.question('Guess the number (1-100): ', number =>
-        {
-            guess = Number(number);
-            if(guess <= 100 && guess >= 0){
-                if(guess == computerGuess){
+    let game = function () {
+        readline.question(`Guess the number (${task})`, (number) => {
+        guess = Number(number);
+        switch(difficulty){
+            case '1': level = (guess <= 100 && guess >= 0); break;
+            case '2': level = (guess <= 150 && guess >= 0); break;
+            case '3': level = (guess <= 200 && guess >= 0); break;
+        }
+            if (level) {
+                if (guess == computerGuess) {
                     console.log("You guessed the number. Congratulations!");
                     return readline.close();
-                } else if(guess < computerGuess){
+                } else if (guess < computerGuess) {
                     console.log("Too Low!");
-                    question();
-                } else if(guess > computerGuess){
+                    game();
+                } else if (guess > computerGuess) {
                     console.log("Too High!");
-                    question();
+                    game();
                 }
-            }   else {
+            } else {
                 console.log("Invalid Input! Try again...");
-                question();
+                game();
             }
-
-        });
-    }
-    question();
+       // }
+       /* if ((level == "level 2")) {
+            if (guess <= 150 && guess >= 0) {
+                if (guess == computerGuess) {
+                    console.log("You guessed the number. Congratulations!");
+                    return readline.close();
+                } else if (guess < computerGuess) {
+                    console.log("Too Low!");
+                    game();
+                } else if (guess > computerGuess) {
+                    console.log("Too High!");
+                    game();
+                }
+            } else {
+                console.log("Invalid Input! Try again...");
+                game();
+            }
+        } */
+    });
+  };
+  game();
 }
-guessANumber()
+guessANumber();
